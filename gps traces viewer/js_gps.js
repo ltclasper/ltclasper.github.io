a1f616246858d5c3882681434e3078d3c1e25758
Luke Clasper <ltclasper@aol.com>
2018-03-09 22:03:18 +0000

    Update js_gps.js

--- gps traces viewer/js_gps.js
+++ gps traces viewer/js_gps.js
@@ -3,54 +3,54 @@
         'Point': new ol.style.Style({
           image: new ol.style.Circle({
             fill: new ol.style.Fill({
-              color: 'Aqua '
+              color: '#FDE511'
             }),
             radius: 5,
             stroke: new ol.style.Stroke({
-              color: 'DimGray',
+              color: '#000000',
               width: 1
             })
           })
         }),
         'LineString': new ol.style.Style({
           stroke: new ol.style.Stroke({
-            color: 'Aqua ',
+            color: '#FDE511',
             width: 3
           })
         }),
         'Polygon': new ol.style.Style({
           fill: new ol.style.Fill({
-            color: 'Aqua '
+            color: '#FDE511'
           }),
           stroke: new ol.style.Stroke({
-            color: 'DimGray',
+            color: '#000000',
             width: 1
           })
         }),
         'MultiPoint': new ol.style.Style({
           image: new ol.style.Circle({
             fill: new ol.style.Fill({
-              color: 'Aqua '
+              color: '#FDE511'
             }),
             radius: 5,
             stroke: new ol.style.Stroke({
-              color: 'DimGray',
+              color: '#000000',
               width: 1
             })
           })
         }),
         'MultiLineString': new ol.style.Style({
           stroke: new ol.style.Stroke({
-            color: 'Aqua ',
+            color: '#FDE511',
             width: 3
           })
         }),
         'MultiPolygon': new ol.style.Style({
           fill: new ol.style.Fill({
-            color: 'Aqua '
+            color: '#FDE511'
           }),
           stroke: new ol.style.Stroke({
-            color: 'DimGray',
+            color: '#000000',
             width: 1
           })
         })
